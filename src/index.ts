ec78f6caa6c9fa1bafba2ff058b9ce09bde311ec
Izoomii <635shadows@gmail.com>
2021-08-10 23:44:59 +0100

    mock database + CR post

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,47 @@ import express from "express"
 const app = express();
 const port = 3000
 
+/**
+ * Users WIP EVERYONE ANON
+ * Subreddits WIP ANYONE CAN JOIN ANY SUBREDDIT
+ * Posts <-
+ */
+
+interface CreatePostPayload {
+    username: string;
+    subreddit: string;
+    message: string;
+}
+
+const database: CreatePostPayload[] = [];
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World2!')
 })
+
+app.get('/izumi', (req, res) => {
+    res.send('Hello izumi!')
+})
+
+app.post('/post', (req, res) => {
+    const body = req.body as CreatePostPayload;
+    database.push(body)
+    res.send(`thnx: ${body.username}`)
+})
+
+app.get('/post', (req, res) => {
+    res.json(database)
+})
+
+app.get('/post/r/:subreddit', (req, res) => {
+    const subreddit = req.params.subreddit;
+    console.log(subreddit)
+    const result = database.filter((value) => value.subreddit === `r/${subreddit}`);
+    res.json(result)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- zb.md
+++ zb.md
@@ -1,4 +1,9 @@
-git remote add origin https://github.com/Izoomii/Redditwo.git
+# HTTP
 
-master slave
-miso mosi
+## POST
+Post requests are usually used for "Posting/Adding" informations to the server.
+## GET
+This is what the browser uses per default.
+Get requests are usually used for "Getting/Querying" information from the server.
+## PUT
+## DELETE
